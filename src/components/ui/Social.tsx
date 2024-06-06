4229fa4454f28f67672f111434d6bf570e6cfb27
Tirthankar Dibbo <tirthankar.sh37@gmail.com>
2024-06-06 11:52:18 +0600

    commit

--- src/components/ui/Social.tsx
+++ src/components/ui/Social.tsx
@@ -1,26 +1,29 @@
-
-import Link from "next/link"
-import {FaGithub, FaLinkedin, FaFacebook, FaBehance} from 'react-icons/fa'
+import React from 'react';
+import Link from "next/link";
+import { FaGithub, FaLinkedin, FaFacebook, FaBehance } from 'react-icons/fa';
 
 const socialLinks = [
-  {icon: <FaLinkedin />, path: 'https://www.linkedin.com/in/tirthankar-saha-dibbya-774685234'},
-  {icon: <FaGithub />, path: 'https://github.com/tirthansaha'},
-  {icon: <FaFacebook />, path: 'https://www.facebook.com/saha.Dibbo'},
-  {icon: <FaBehance />, path: 'https://www.behance.net/tirthansaha'},
+  { icon: <FaLinkedin />, path: 'https://www.linkedin.com/in/tirthankar-saha-dibbya-774685234' },
+  { icon: <FaGithub />, path: 'https://github.com/tirthansaha' },
+  { icon: <FaFacebook />, path: 'https://www.facebook.com/saha.Dibbo' },
+  { icon: <FaBehance />, path: 'https://www.behance.net/tirthansaha' },
 ];
 
-const Social=({containerStyles, iconStyles}) => {
-    return (
-    <div className={containerStyles}>
-    {socialLinks.map((item, index)=>{
-        return(
-        <Link key={index} href={item.path} className={iconStyles}>
-            {item.icon}
-        </Link>
-        )
-    })}
-    </div>
-    )
+interface SocialProps {
+  containerStyles?: string;
+  iconStyles?: string;
 }
 
-export default Social
+const Social: React.FC<SocialProps> = ({ containerStyles = '', iconStyles = '' }) => {
+  return (
+    <div className={containerStyles}>
+      {socialLinks.map((item, index) => (
+        <Link key={index} href={item.path} className={iconStyles}>
+          {item.icon}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Social;
